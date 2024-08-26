649cddea07ca5234866fe77f1b5e46e02a714770
Ashutosh Patil <ashutosh.patil@sportzinteractive.com>
2024-08-26 11:28:16 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>From app</h1>
+  <CounterComponent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CounterComponent from './components/Counter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CounterComponent // Updated to match the new component name
   }
 }
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>Counter</h1>
+      <p>Counter: {{ $store.state.counter }}</p>
+      <button @click="$store.commit('increment')">Increment</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CounterComponent' // Updated to a multi-word name
+  }
+  </script>
+  
+  <style scoped>
+  </style>
+  
